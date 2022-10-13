59d27f8a4f81843c5d3d8c5d51bdcc80ca123821
Kamal Walia <kamalw1709@gmail.com>
2022-10-13 19:58:55 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,174 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useRef } from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Button,
+  useColorScheme,
+  View,
+  Platform
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { WebView } from 'react-native-webview';
+import RNHTMLtoPDF from 'react-native-html-to-pdf';
+import RNPrint from 'react-native-print';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+class App extends React.Component {
+  state = {
+    selectedPrinter: null
+  }
+
+  // @NOTE iOS Only
+  selectPrinter = async () => {
+    const selectedPrinter = await RNPrint.selectPrinter({ x: 100, y: 100 })
+    this.setState({ selectedPrinter })
+  }
+
+  // @NOTE iOS Only
+  silentPrint = async () => {
+    if (!this.state.selectedPrinter) {
+      alert('Must Select Printer First')
+    }
+
+    const jobName = await RNPrint.print({
+      printerURL: this.state.selectedPrinter.url,
+      html: '<h1>Silent Print</h1>'
+    })
+
+  }
+
+  async printHTML() {
+    const response = await fetch('https://www.folkstalk.com/2022/07/get-html-from-url-in-react-js-with-code-examples.html');
+    const template = await response.text();
+    await RNPrint.print({
+      html: template
+    })
+  }
+
+  async printPDF() {
+    const results = await RNHTMLtoPDF.convert({
+      html: '<h1>Custom converted PDF Document</h1>',
+      fileName: 'test',
+      base64: true,
+    })
+
+    await RNPrint.print({ filePath: results.filePath })
+  }
+
+  async printRemotePDF() {
+    await RNPrint.print({ filePath: 'https://graduateland.com/api/v2/users/jesper/cv' })
+  }
+
+
+  //  handlePrint = () => {
+  //   browserRef.current.injectJavaScript("window.print()");
+  //  window.print()
+    
+  // }
+
+  customOptions = () => {
+    return (
+      <View>
+        {this.state.selectedPrinter &&
+          <View>
+            <Text>{`Selected Printer Name: ${this.state.selectedPrinter.name}`}</Text>
+            <Text>{`Selected Printer URI: ${this.state.selectedPrinter.url}`}</Text>
+          </View>
+        }
+      <Button onPress={this.selectPrinter} title="Select Printer" />
+      <Button onPress={this.silentPrint} title="Silent Print" />
+    </View>
+
+    )
+  }
+
+  render(){
+    return (
+    //  <SafeAreaView style={{flex: 1}}>
+    //    <TouchableOpacity onPress={handlePrint}><Text>Print</Text></TouchableOpacity> */}
+    //     <WebView source={{uri: 'https://www.google.com/'}} /> 
+          <View style={styles.container}>
+        {/* {Platform.OS === 'ios' && this.customOptions()}*/}
+        <Button onPress={this.printHTML} title="Print HTML" />
+        <Button onPress={this.printPDF} title="Print PDF" />
+        <Button onPress={this.printRemotePDF} title="Print Remote PDF" /> 
+      </View>
+        // </SafeAreaView>
+    );
+  }
+  
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'yellow',
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
